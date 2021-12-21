bf241054385178d7075e81530ee6cd2b9509fc68
Andre Silva <wss.andre@gmail.com>
2021-12-21 15:51:30 -0300

    Restarted project with ASP.NET Core API instead of Flask ¯\_(ツ)_/¯

--- app/model/user.py
+++
@@ -1,9 +0,0 @@
-class User:
-    id: int
-    name: str
-    password: str
-
-    def __init__(self, id, username, password) -> None:
-        self.id = id
-        self.username = username
-        self.password = password
