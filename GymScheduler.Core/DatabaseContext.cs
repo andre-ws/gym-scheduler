ac61c8f2d49d3b3769da21fcecbef9cba30a492a
Andre Silva <wss.andre@gmail.com>
2021-12-21 18:23:47 -0300

    Added DbContext configuration

--- GymScheduler.Core/DatabaseContext.cs
+++ GymScheduler.Core/DatabaseContext.cs
@@ -1,9 +1,28 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using GymScheduler.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace GymScheduler.Core
 {
-    public class DatabaseContext : DbContext
+    public class GymSchedulerContext : DbContext
     {
-        //public DbSet<>
+        public GymSchedulerContext(DbContextOptions<GymSchedulerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Class> Classes { get; set; }
+        public DbSet<ClassType> ClassTypes { get; set; }
+        public DbSet<MatRoom> MatRooms { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Class>().ToTable("Class");
+            modelBuilder.Entity<ClassType>().ToTable("ClassType");
+            modelBuilder.Entity<MatRoom>().ToTable("MatRoom");
+            modelBuilder.Entity<Teacher>().ToTable("Teacher");
+            modelBuilder.Entity<User>().ToTable("User");
+        }
     }
 }
