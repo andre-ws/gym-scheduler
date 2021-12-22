e52694e82a96cae0b7fad572c1dee7d43018f113
Andre Silva <wss.andre@gmail.com>
2021-12-22 15:14:35 -0300

    Added SQLite database support

---
+++ GymScheduler/GymSchedulerDbContext.cs
@@ -0,0 +1,32 @@
+﻿using GymScheduler.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymScheduler
+{
+    public class GymSchedulerDbContext : DbContext
+    {
+        public string DbPath { get; }
+
+        public DbSet<Class> Classes { get; set; }
+        public DbSet<ClassType> ClassTypes { get; set; }
+        public DbSet<MatRoom> MatRooms { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public GymSchedulerDbContext()
+        {
+            // TODO: Move configuration to user secrets file
+            // https://docs.microsoft.com/en-us/ef/core/miscellaneous/connection-strings
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = Path.Join(path, "gym-scheduler.sqlite");
+
+            Database.EnsureCreated();
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+}
